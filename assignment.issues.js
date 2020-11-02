bdb01141fd50721cf3e731ca0fe73ad57e7e086a
Arushi Gupta <67069824+arushi2103@users.noreply.github.com>
2020-11-02 12:28:03 +0530

    added files

---
+++ assignment.issues.js
@@ -0,0 +1,43 @@
+function showName() {
+   var name=prompt("enter your name ");
+   data= document.getElementById('uname');
+    data.innerHTML= name.value;
+}
+
+function calculateNum(num_1, num_2, calc_type) {
+    /** Calculate two numbers as per calulation type, and show result in the text box */
+    var result;
+    switch (calc_type) {
+        case 'add':
+            result = Number(num_1) + Number(num_2);
+         break;
+         case 'minus':
+            result = Number(num_1) - Number(num_2);
+        case 'mul':
+            result = Number(num_1) * Number(num_2);
+        case 'div':
+            result = Number(num_1) / Number(num_2);
+        case 'mod':
+            result = Number(num_1) % Number(num_2);
+    }
+
+    document.getElementById('output_box').value = result;
+}
+
+function changeTitle() {
+    var all_titles = document.getElementsByClassName('title-text');
+    for (var val of all_titles){
+    val.innerHTML = "Successfully Changed titles";
+    }
+ }
+
+function printPage() {
+    var choice = confirm("Do you want to print?");
+    if (choice) {
+        window.print();
+    }
+}
+
+function greetUser() {
+    alert ("Hello and Welcome to our website");
+}
